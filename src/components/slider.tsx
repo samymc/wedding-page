341f19ea241cac847217d863a1a31ce157456f7b
Samy Mori <smoricub@gmail.com>
2024-09-20 01:34:59 -0500

    PDF Viewer/Downloader

---
+++ src/components/slider.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import image1 from "../assets/1.jpg"
+import image2 from "../assets/2.jpg"
+
+const ImageSlider = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const images = [
+    image1,
+    image2
+  ];
+  const goToImage = (index: number) => {
+    setCurrentImage(index);
+  };
+
+  return (
+    <div className="slider-container">
+      <img src={images[currentImage]} alt={`Imagen ${currentImage + 1}`} className="slider-image" />
+      <div className="slider-controls">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            className={`slider-dot ${currentImage === index ? 'active' : ''}`}
+            onClick={() => goToImage(index)}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider;
--- src/pages/details/Details.tsx
+++ src/pages/details/Details.tsx
@@ -1,8 +1,9 @@
+import { Section1 } from "./views/section1";
 
 export const Details = () => {
   return (
-    <>
-      
-    </>
+    <div className="details">
+      <Section1/>
+    </div>
   );
 };
